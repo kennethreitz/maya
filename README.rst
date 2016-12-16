98a74a11365997a98f228f8719f8ee2a391e9dce
Kenneth Reitz <me@kennethreitz.org>
2016-12-15 20:06:54 -0500

    getting there

--- README.rst
+++ README.rst
@@ -13,11 +13,13 @@ Note that outputs are fuzzy and made up, just playing with an API here::
     >>> tomorrow = now.forwards('one day')
     <MayaDT epoch=1481977021>
 
-    >>> tomorrow.iso8601
+    >>> tomorrow.iso8601()
     2016-12-16T00:12:54+00:00
 
-    >>> tomorrrow.iso8601
+    >>> tomorrrow.iso8601()
     Fri, 17 Dec 2016 00:12:54 +0000
 
-    >>> tomorrow = maya.import('tomorrow').datetime
+    >>> tomorrow = maya.adjust('tomorrow').datetime
     datetime.datetime(2016, 12, 16, 0, 14, 33, 950436)
+
+Timezones fit in here somewhere.
---
+++ maya.py
@@ -0,0 +1,50 @@
+
+# ___  __  ___  _  _  ___
+# || \/ | ||=|| \\// ||=||
+# ||    | || ||  //  || ||
+
+# Ignore warnings for yaml usage.
+import warnings
+import ruamel.yaml
+warnings.simplefilter('ignore', ruamel.yaml.error.UnsafeLoaderWarning)
+
+import email.utils
+import time
+from datetime import datetime as Datetime
+
+import dateparser
+
+class MayaDT(object):
+    """The Maya Datetime object."""
+    def __init__(self, epoch):
+        super(MayaDT, self).__init__()
+        self._epoch = epoch
+
+    def __repr__(self):
+        return '<MayaDT epoch={}>'.format(self._epoch)
+
+    def datetime(self):
+        return Datetime.fromtimestamp(self._epoch)
+
+    def iso8601(self):
+        return '{}Z'.format(self.datetime().isoformat())
+
+    def rfc2822(self):
+        tt = self.datetime().timetuple()
+        ts = time.mktime(tt)
+        return email.utils.formatdate(ts)
+
+
+    def adjust(adjustment, positive=True):
+        pass
+
+
+def now():
+    """Returns MayaDT for right now."""
+    epoch = time.time()
+    return MayaDT(epoch=epoch)
+
+def parse(string):
+    dt = dateparser.parse(string)
+    epoch = (dt - Datetime(1970, 1, 1)).total_seconds()
+    return MayaDT(epoch)
