983e7fd6f2084e1dedea7985a2ee756991e56a42
Kenneth Reitz <me@kennethreitz.org>
2016-12-16 13:34:14 -0500

    basic setup.py (not valid yet)

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from setuptools import setup
+
+
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist bdist_wheel upload")
+    sys.exit()
+
+required = []
+
+setup(
+    name='maya',
+    version='0.0.0',
+    description='Datetimes for Humans.',
+    long_description=open('README.rst').read(),
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    url='https://github.com/kennethreitz/maya',
+    my_modules= ['maya'],
+    install_requires=required,
+    license='MIT',
+    classifiers=(
+    ),
+)
