6fbf8f2d7410173eeba5cbfdb349c52ab055471c
Kenneth Reitz <me@kennethreitz.org>
2017-05-23 11:38:09 -0700

    fix the bug

--- maya.py
+++ maya.py
@@ -335,7 +335,7 @@ class MayaInterval(object):
 
         # # Start and duration, such as "2007-03-01T13:00:00Z/P1Y2M10DT2H30M"
         # # Duration and end, such as "P1Y2M10DT2H30M/2008-05-11T15:30:00Z"
-        raise NotImplentedError()
+        raise NotImplementedError()
 
 
     def __and__(self, i):
