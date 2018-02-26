3cb65f227c70f6a76368a1889ad59800d27fe743
Kenneth Reitz <me@kennethreitz.org>
2018-02-26 17:57:57 -0500

    setup.py update
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import io
 import os
 import sys
 import codecs
+from shutil import rmtree
 
-from setuptools import setup
+from setuptools import find_packages, setup, Command
 
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -30,6 +32,43 @@ required = [
 packages = [
     'maya',
 ]
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
 
 # About dict to store version and package info
 about = dict()
@@ -61,4 +100,7 @@ setup(
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ),
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
